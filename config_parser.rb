a585b77ed213c4faa805efbbbfbeb3d053ccd45b
ealvaro <alvaroescobaronline@gmail.com>
2021-04-07 22:31:25 -0400

    Added the picky changes requested by the interviewer

--- config_parser.rb
+++ config_parser.rb
@@ -1,32 +1,30 @@
-require 'json'
+# This class parses the external and generates a hash with the information
 
+# frozen_string_literal: true
+
+# Class with the parse(filename) public method
 class ConfigParser
-
-  FALSE_VALUES = [false, 0, "0", "f", "false", "off", "no"]
-  TRUE_VALUES = [true, 1, "1", "t", "true", "on", "yes"]
+  FALSE_VALUES = [false, 'false', 'off', 'no'].freeze
+  TRUE_VALUES = [true, 'true', 'on', 'yes'].freeze
 
   def self.parse(file_name)
     file_data = {}
-    File.open(file_name, 'r') do |file|
-      file.each_line do |line|
-        if line[0] != '#' then                                       #ignore comments
-          line_data = line.split('=')                                #break line into key and value
-          file_data[line_data[0]] = format_types(line_data[1].strip) #get rid of spaces and \n and format output
-        end
-      end
-    end
-    return file_data
-  end
+    File.open(file_name, 'r').each_line do |line|
+      next if line[0] == '#'      # ignore comments
 
-  private
+      line_data = line.split('=') # break line into key and value
+      file_data[line_data[0].strip] = format_types(line_data[1].strip) # get rid of spaces and \n and format output
+    end
+    file_data
+  end
 
   # Converts strings into its correct data type depending on values
   def self.format_types(str)
     return true if TRUE_VALUES.include? str.downcase
     return false if FALSE_VALUES.include? str.downcase
-    return str.to_f if (str.to_f % 1) > 0
+    return str.to_f if (str.to_f % 1).positive?
     return str.to_i if str.to_i.to_s == str
-    return str
-  end
 
+    str
+  end
 end
--- config_parser_spec.rb
+++ config_parser_spec.rb
@@ -1,49 +1,50 @@
-require_relative 'config_parser'
+# This class will test the config_parser class
+# frozen_string_literal: true
 
-describe ConfigParser do
+require_relative 'config_parser'
+RSpec.describe ConfigParser do
   describe '.parse' do
     context 'when parse is called on data.txt' do
       let(:file) { 'data.txt' }
 
       it 'returns the provided data.txt file as a Hash' do
-        expect(ConfigParser.parse(file)).to eq({
-          "host "=>"test.com",
-          "server_id"=>55331,
-          "cost"=>2.56,
-          "user"=>"user",
-          "verbose "=>true,
-          "test_mode "=>true,
-          "debug_mode "=>false,
-          "log_file_path "=>"/tmp/logfile.log",
-          "send_notifications "=>true
-        })
+        expect(ConfigParser.parse(file)).to eq(
+          { 'host' => 'test.com',
+            'server_id' => 55_331,
+            'cost' => 2.56,
+            'user' => 'user',
+            'verbose' => true,
+            'test_mode' => true,
+            'debug_mode' => false,
+            'log_file_path' => '/tmp/logfile.log',
+            'send_notifications' => true }
+        )
       end
     end
   end
 
   describe '.format_types' do
     context 'when different data types are converted' do
-
-      it "tests for true values" do
-        expect(ConfigParser.send(:format_types, "on")).to eq(true)
-        expect(ConfigParser.send(:format_types, "True")).to eq(true)
-        expect(ConfigParser.send(:format_types, "YES")).to eq(true)
+      it 'tests for true values' do
+        expect(ConfigParser.send(:format_types, 'on')).to eq(true)
+        expect(ConfigParser.send(:format_types, 'True')).to eq(true)
+        expect(ConfigParser.send(:format_types, 'YES')).to eq(true)
       end
 
-      it "tests for false values" do
-        expect(ConfigParser.send(:format_types, "Off")).to eq(false)
-        expect(ConfigParser.send(:format_types, "false")).to eq(false)
-        expect(ConfigParser.send(:format_types, "No")).to eq(false)
+      it 'tests for false values' do
+        expect(ConfigParser.send(:format_types, 'Off')).to eq(false)
+        expect(ConfigParser.send(:format_types, 'false')).to eq(false)
+        expect(ConfigParser.send(:format_types, 'No')).to eq(false)
       end
 
-      it "tests for floating values" do
-        expect(ConfigParser.send(:format_types, "123.45")).to eq(123.45)
-        expect(ConfigParser.send(:format_types, "0.0333")).to eq(0.0333)
+      it 'tests for floating values' do
+        expect(ConfigParser.send(:format_types, '123.45')).to eq(123.45)
+        expect(ConfigParser.send(:format_types, '0.0333')).to eq(0.0333)
       end
 
-      it "tests for integer values" do
-        expect(ConfigParser.send(:format_types, "12345")).to eq(12345)
-        expect(ConfigParser.send(:format_types, "-3467")).to eq(-3467)
+      it 'tests for integer values' do
+        expect(ConfigParser.send(:format_types, '12345')).to eq(12_345)
+        expect(ConfigParser.send(:format_types, '-3467')).to eq(-3467)
       end
     end
   end
